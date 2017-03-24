7d2bd28cde76d63f791460a6ac089860b2ff0d2f
Emma Lysek <emmalysek@gmail.com>
2017-03-24 16:33:57 -0400

    creates locationDataCollector class with public and private methods

--- r_app/r_app/static/main.js
+++ r_app/r_app/static/main.js
@@ -1,1 +1,10 @@
-$(".contact_desk_name").css("color", "red");
+// Main Core File
+
+var initialize = function() {
+    var o = RAPP.LocationDataCollector.getLocationIdsToHoursIdsObject( );
+    var p = RAPP.LocationDataCollector.getHourIdtoHourValueObject( );
+    console.log(o);
+    console.log(p);
+}
+
+$(document).ready(initialize);
